7ccab83f2493ca8cb5b88a4e4082540aecd8d68c
demisu-s <damisusefu@gmail.com>
2024-01-26 22:01:10 +0300

    add tailwind cards

---
+++ client/src/Components/PropertiesHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PropertiesHeader = () => {
+  return (
+    <div className='mx-auto max-w-sm flex flex-col justify-center my-12'>
+      <h1 className='text-center text-2xl font-bold mb-12'>Product from Oxford</h1>
+    
+    </div>
+  );
+};
+
+export default PropertiesHeader;
---
+++ client/src/Pages/PropertiesForSale.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropertiesCard from '../Components/PropertiesCard';
+import { Link } from 'react-router-dom';
+
+const PropertiesForSale = () => {
+  return (
+    <div className='bg-[hsl(0,14%,96%)] max-w-[1000px] flex flex-col justify-center my-10 mx-auto  rounded-md rounded-b-none h-fit'>
+      <div className='block'>
+        <div className='flex justify-between items-center'>
+          <div>
+            <h1 className='mx-0 ml-2 mt-2 text-1xl font-bold'>Houses from Oxford</h1>
+            <h3 className='ml-2 mb-8'>604 results</h3>
+          </div>
+          <div>
+            <Link to='/somewhere' className='mr-2 underline'>Sort most recent</Link>
+          </div>
+        </div>
+      </div>
+
+      <PropertiesCard />
+    </div>
+  );
+};
+
+export default PropertiesForSale;
